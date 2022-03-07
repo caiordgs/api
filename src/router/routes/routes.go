7cde0ea81ac4f10afe7ba3b0d99f61645f022fd3
Caio <caiordgs@outlook.com>
2022-03-07 20:31:51 -0300

    updated routes.go and router.go with cleaner coding.

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,10 +1,24 @@
 package routes
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 type Route struct {
 	URI         string
 	Method      string
 	Function    func(http.ResponseWriter, *http.Request)
 	RequireAuth bool
+}
+
+// Sets all routes inside the router.
+func Config(r *mux.Router) *mux.Router {
+	routes := userRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
