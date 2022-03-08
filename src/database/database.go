8b7951ef231115418df40b5ab6db9adfa304af39
Caio <caiordgs@outlook.com>
2022-03-08 19:32:57 -0300

    Created database.go with database connection.

---
+++ src/database/database.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"api/src/config"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql" // Driver
+)
+
+func Connect() (*sql.DB, error) {
+	db, erro := sql.Open("mysql", config.DatabaseConnectionString)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		db.Close()
+		return nil, erro
+	}
+
+	return db, nil
+}
