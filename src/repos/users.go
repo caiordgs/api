b0536462d080c50f03790254ff3eb7653e316b89
Caio <caiordgs@outlook.com>
2022-03-09 20:22:25 -0300

    Added create user repository with SQL statements.

--- src/repos/users.go
+++ src/repos/users.go
@@ -13,6 +13,24 @@ func NewUserRepository(db *sql.DB) *Users {
 	return &Users{db}
 }
 
-func (u Users) Create(user models.User) (uint64, error) {
-	return 0, nil
+func (repository Users) Create(user models.User) (uint64, error) {
+	statement, erro := repository.db.Prepare(
+		"insert into users (name, nick, email, password) values(?, ?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	result, erro := statement.Exec(user.Name, user.Nick, user.Email, user.Password)
+	if erro != nil {
+		return 0, erro
+	}
+
+	lastInsertedID, erro := result.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(lastInsertedID), nil
 }
