c92d7cd8857388ff77aae5602ad3093be047db49
Caio <caiordgs@outlook.com>
2022-03-15 06:49:20 -0300

    Validating user creation.

--- src/models/User.go
+++ src/models/User.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +13,39 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedIn time.Time `json:"createdIn,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	if erro := user.validate(); erro != nil {
+		return erro
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("O nome é obrigatório.")
+	}
+
+	if user.Nick == "" {
+		return errors.New("O nick é obrigatório.")
+	}
+
+	if user.Email == "" {
+		return errors.New("O e-mail é obrigatório.")
+	}
+
+	if user.Password == "" {
+		return errors.New("A senha é obrigatório.")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
