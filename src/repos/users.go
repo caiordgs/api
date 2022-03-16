1c9e21638a8e91b3818c2e27568d427928347d65
Caio <caiordgs@outlook.com>
2022-03-16 18:49:11 -0300

    Created repository so we're able to search for an user by his/her name or nickname.

--- src/repos/users.go
+++ src/repos/users.go
@@ -3,6 +3,7 @@ package repos
 import (
 	"api/src/models"
 	"database/sql"
+	"fmt"
 )
 
 type Users struct {
@@ -33,4 +34,38 @@ func (repository Users) Create(user models.User) (uint64, error) {
 	}
 
 	return uint64(lastInsertedID), nil
+}
+
+func (repository Users) Search(nameOrNick string) ([]models.User, error) {
+	nameOrNick = fmt.Sprintf("%%%s%%", nameOrNick)
+
+	lines, erro := repository.db.Query(
+		"select id, name, nick, email, createdIn from users where name LIKE ? or nick LIKE ?",
+		nameOrNick, nameOrNick,
+	)
+
+	if erro != nil {
+		return nil, erro
+	}
+
+	defer lines.Close()
+
+	var users []models.User
+
+	for lines.Next() {
+		var user models.User
+
+		if erro = lines.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Nick,
+			&user.Email,
+		); erro != nil {
+			return nil, erro
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }
