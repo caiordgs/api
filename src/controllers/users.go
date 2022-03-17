488bead948fcdf637b08fb06f428ec81b0b7f9f6
Caio <caiordgs@outlook.com>
2022-03-17 08:08:18 -0300

    Update User function added.

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -26,7 +26,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if erro = user.Prepare(); erro != nil {
+	if erro = user.Prepare("register"); erro != nil {
 		responses.Erro(w, http.StatusBadRequest, erro)
 		return
 	}
@@ -70,9 +70,9 @@ func SearchUsers(w http.ResponseWriter, r *http.Request) {
 
 // Searches one specific user.
 func SearchUser(w http.ResponseWriter, r *http.Request) {
-	parametros := mux.Vars(r)
+	parameters := mux.Vars(r)
 
-	userID, erro := strconv.ParseUint(parametros["userId"], 10, 64)
+	userID, erro := strconv.ParseUint(parameters["userId"], 10, 64)
 	if erro != nil {
 		responses.Erro(w, http.StatusBadRequest, erro)
 		return
@@ -96,7 +96,43 @@ func SearchUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Updating user!"))
+	parameters := mux.Vars(r)
+	userID, erro := strconv.ParseUint(parameters["userId"], 10, 64)
+	if erro != nil {
+		responses.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	requestBody, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		responses.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var user models.User
+	if erro = json.Unmarshal(requestBody, &user); erro != nil {
+		responses.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = user.Prepare("edicao"); erro != nil {
+		responses.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := database.Connect()
+	if erro != nil {
+		responses.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repos := repos.NewUserRepository(db)
+	if erro = repos.Update(userID, user); erro != nil {
+		responses.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	responses.JSON(w, http.StatusNoContent, nil)
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
