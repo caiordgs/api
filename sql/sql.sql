b787de282d4916f73a58bd59b9e29b7d5c3c3d2a
Caio <caiordgs@outlook.com>
2022-03-21 20:36:47 -0300

    Changed password size.

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
-DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
     id int auto_increment primary key,
     name varchar(50) not null,
     nick varchar(50) not null unique,
     email varchar(50) not null unique,
-    password varchar(20) not null,
+    password varchar(100) not null,
     createdIn timestamp default current_timestamp()
 ) ENGINE=INNODB;
