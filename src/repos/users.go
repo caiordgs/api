044b944cfa803b9bdaa104d82262abe888665bd7
Caio <caiordgs@outlook.com>
2022-03-21 20:06:32 -0300

    Added func Delete.

--- src/repos/users.go
+++ src/repos/users.go
@@ -111,3 +111,17 @@ func (repository Users) Update(ID uint64, user models.User) error {
 
 	return nil
 }
+
+func (repository Users) Delete(ID uint64) error {
+	statement, erro := repository.db.Prepare("delete from users where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
