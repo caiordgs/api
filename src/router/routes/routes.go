506ce63207ff1962c9489bc15f434c3719aaa43a
Caio <caiordgs@outlook.com>
2022-03-22 07:06:47 -0300

    Added middlewares to the routes.

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"api/src/middlewares"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -18,7 +19,14 @@ func Config(r *mux.Router) *mux.Router {
 	routes = append(routes, loginRoute)
 
 	for _, route := range routes {
-		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+
+		if route.RequireAuth {
+			r.HandleFunc(route.URI,
+				middlewares.Logger(middlewares.Authenticate(route.Function)),
+			).Methods(route.Method)
+		} else {
+			r.HandleFunc(route.URI, middlewares.Logger(route.Function)).Methods(route.Method)
+		}
 	}
 
 	return r
