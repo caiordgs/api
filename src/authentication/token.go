5aa1f036ae88c06cd194fa483c2d3b9747d2e059
Caio <caiordgs@outlook.com>
2022-03-21 21:27:31 -0300

    Imported package github.com/dgrijalva/jwt-go and added token permissions. Not yet finished, will add a more complicated signature.

---
+++ src/authentication/token.go
@@ -0,0 +1,15 @@
+package authentication
+
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+func CreateToken(userID uint64) (string, error) {
+	permissions := jwt.MapClaims{}
+	permissions["authorized"] = true
+	permissions["exp"] = time.Now().Add(time.Hour * 6).Unix()
+	permissions["userId"] = userID
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
+	return token.SignedString([]byte("Secret"))
+}
