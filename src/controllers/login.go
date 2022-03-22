ef0aff00c352f56e5bc289ef8e366427883ebdbc
Caio <caiordgs@outlook.com>
2022-03-21 21:08:45 -0300

    Login controller, not yet finished.

---
+++ src/controllers/login.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repos"
+	"api/src/responses"
+	"api/src/security"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+// Login is responsible for the validation of a user.
+func Login(w http.ResponseWriter, r *http.Request) {
+	requestBody, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		responses.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+	var user models.User
+	if erro = json.Unmarshal(requestBody, &user); erro != nil {
+		responses.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := database.Connect()
+	if erro != nil {
+		responses.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repository := repos.NewUserRepository(db)
+	// savedUser is the user saved in the database.
+	savedUser, erro := repository.SearchByEmail(user.Email)
+	if erro != nil {
+		responses.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if erro = security.PasswordValidation(savedUser.Password, user.Password); erro != nil {
+		responses.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+	w.Write([]byte("You are logged in."))
+}
