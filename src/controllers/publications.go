ccc6e0043c3afe5f4c2adeaa5831808470263a14
Caio <caiordgs@outlook.com>
2022-03-23 21:27:41 -0300

    Wrote all the controllers for the publications. WIP.

---
+++ src/controllers/publications.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CreatePost creates a post on the database.
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// FindPosts brings up the posts that show up on the user feed.
+func FindPosts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// FindPost brings up a specific post.
+func FindPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// UpdatePost modifies a post.
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletePost deletes a post.
+func DeletePost(w http.ResponseWriter, r *http.Request) {
+
+}
